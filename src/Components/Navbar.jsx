ee1c890f73695d770bdbbfd4e6636d1c82e5bd7c
Riya1509 <riya.m1509@gmail.com>
2024-05-13 19:36:16 +0530

    made some changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <>
       <div className="container nav_bar" data-aos="fade-down" data-aos-duration="1000">
-        <div className="left nav_items">Porfolio</div>
+        <div className="left nav_items">Portfolio</div>
         <div className="right">
           <a href="#home" className="nav_items">Home</a>
           <a href="#skills" className="nav_items">Skills</a>
